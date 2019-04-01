a49f06edaf6a5a3b5b97b26f8be2e75edcb40491
sysysysy242 <45331186+sysysysy242@users.noreply.github.com>
2019-04-01 18:18:33 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -133,19 +133,18 @@ if(message.content === prefix + "help") {
       if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.channel.send("Vous n'avez pas la permission!");
   
       if(message.mentions.users.size === 0) {
-          return message.channel.send("Vous devez metionner un utilisaeur")
+          return message.channel.send("Vous devez metionner un utilisaeur");
       }
+
       var kick = message.guild.member(message.mentions.users.first());
       if(!kick) {
-          return message.channel.send("Je ne sais pas si l'utilisateur existe :/")
+          return message.channel.send("Je ne sais pas si l'utilisateur existe :/");
       }
   
-      if(message.guild.member(client.user).hasPermission("KICK_MEMBERS")) {
-          return message.channel.send("Je n'ai pas la permission pour kick");
-      }
   
       kick.kick().then(member => {
           message.channel.send(`${member.user.username} est kick pas ${message.author.username}`);
+          message.mentions.users.first().send(`Vous êtes kick du serveur ${message.guild.name}`);
       });
   }
 
